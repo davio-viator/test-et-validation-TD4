1649d30d84afd80e5fb36d95928933f9690b8c81
Dorian <dorian.taallah@etu.umontpellier.fr>
2020-02-28 17:02:12 +0100

    Matrice v1

--- src/Matrice.java
+++ src/Matrice.java
@@ -1,11 +1,19 @@
+import java.util.Arrays;
+
 public class Matrice {
 
-    public static double[][] matrice(int m, int n, double a, double b){
-        return new double[5][5];
+    public static int[][] matrice(int m, int n, int a, int b) {
+        int[][] mat = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                mat[i][j] = a + (int)(Math.random() * ((b - a) + 1));
+            }
+        }
+        return mat;
     }
 
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        int[][] mat = matrice(3,5,1,1000);
+        System.out.println(Arrays.deepToString(mat));
     }
 }
